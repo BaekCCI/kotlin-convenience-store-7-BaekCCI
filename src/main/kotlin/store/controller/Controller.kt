21323ec1b7ae7ec53cc289d52cf4b0b9084b319d
baekcii <bsy8470@gmail.com>
2024-11-07 04:15:02 +0900

    refactor(productManagement): 상품 목록 초기화 방식 수정

---
+++ src/main/kotlin/store/controller/Controller.kt
@@ -0,0 +1,9 @@
+package store.controller
+
+import store.model.ProductManagement
+
+class Controller {
+    fun start(){
+        val productManagement=ProductManagement()
+    }
+}
--- src/main/kotlin/store/model/ProductManagement.kt
+++ src/main/kotlin/store/model/ProductManagement.kt
@@ -3,20 +3,22 @@ package store.model
 const val INVENTORY_PATH = "src/main/resources/products.md"
 
 //재고 관리 클래스
-class ProductManagement(private val products : MutableList<Product>){
+class ProductManagement() {
+    private val products: List<Product>
+
     init {
         val inventory = java.io.File(INVENTORY_PATH).readLines()
-        inventory.forEach {
-            products.add(getProductInfo(it))
-        }
+        products = inventory.map { getProductInfo(it) }
     }
-    private fun getProductInfo(productInfo : String) : Product{
-        val product = productInfo.split(",").map{it.trim()}
+
+    private fun getProductInfo(productInfo: String): Product {
+        val product = productInfo.split(",").map { it.trim() }
         return Product(
             name = product[0],
             price = product[1].toInt(),
             quantity = product[2].toInt(),
-            promotion = product[3])
+            promotion = product[3]
+        )
     }
 
     //상품 구매 시 상품 재고 업데이트 로직
