9e43429629e7d66c8dee4946d3960137f2531cf1
baekcii <bsy8470@gmail.com>
2024-11-08 14:39:04 +0900

    refactor(inputView):enum class 프로퍼티 private 선언

--- src/main/kotlin/store/view/InputView.kt
+++ src/main/kotlin/store/view/InputView.kt
@@ -2,7 +2,7 @@ package store.view
 
 import camp.nextstep.edu.missionutils.Console
 
-enum class InputMessage(val message: String) {
+enum class InputMessage(private val message: String) {
     PURCHASE_PRODUCT("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
     PROMOTION_EXTRA_ITEM("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
     PROMOTION_NO_DISCOUNT("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
@@ -17,7 +17,6 @@ enum class InputMessage(val message: String) {
         return message.format(name, noDiscountQuantity)
     }
 }
-
 
 class InputView {
     fun readItem(): String {
