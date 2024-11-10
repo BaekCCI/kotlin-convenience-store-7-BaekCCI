53199dbefa590af11d99872bac76e9923fa6562a
baekcii <bsy8470@gmail.com>
2024-11-10 21:03:33 +0900

    feat(product): 전체 재고를 저장할 변수 추가

--- src/main/kotlin/store/model/Product.kt
+++ src/main/kotlin/store/model/Product.kt
@@ -1,9 +1,10 @@
 package store.model
 
 data class Product(
-    val name : String,
-    var price : Int,
-    var quantity : Int,
-    var promotion : String?
-){
+    val name: String,
+    var price: Int,
+    var entireQuantity: Int,
+    var promotionQuantity: Int,
+    var promotion: String?
+) {
 }
