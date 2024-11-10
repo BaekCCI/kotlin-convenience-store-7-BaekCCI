b986990e008e52096df18c8dcd089960c2177870
baekcii <bsy8470@gmail.com>
2024-11-10 21:27:52 +0900

    refactor(purchase): 기능 분리

--- src/main/kotlin/store/model/Purchase.kt
+++ src/main/kotlin/store/model/Purchase.kt
@@ -10,24 +10,36 @@ class Purchase(val items: String, val productManagement: ProductManagement) {
         addItemToCart()
     }
 
-    fun validateItems(): Boolean {
+    private fun validateItems(): Boolean {
         val regex = Regex(ITEM_REGEX)
         val trimmedItems = items.replace(" ", "")
         return trimmedItems.matches(regex)
     }
 
-    fun addItemToCart() {
+    private fun addItemToCart() {
+        val eachItem = parseItem()
+        val tempCart = buildTempCart(eachItem)
+        updateCart(tempCart)
+    }
+
+    private fun parseItem(): List<String> {
         val cleanedItems = items.replace("[", "").replace("]", "")
-        val eachItem = cleanedItems.split(",").map { it.trim() }
+        return cleanedItems.split(",").map { it.trim() }
+    }
+
+    private fun buildTempCart(eachItem: List<String>): MutableMap<String, Int> {
         val tempCart: MutableMap<String, Int> = mutableMapOf()
         eachItem.forEach {
             val itemParts = it.split("-").map { it.trim() }
             val name = itemParts[0]
             val quantity = itemParts[1].toInt()
             require(productManagement.checkItemInInventory(name)) { "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요." }
-
             tempCart[name] = tempCart.getOrDefault(name, 0) + quantity
         }
+        return tempCart
+    }
+
+    private fun updateCart(tempCart: MutableMap<String, Int>) {
         tempCart.forEach { (name, quantity) ->
             require(
                 productManagement.checkItemQuantity(
@@ -35,7 +47,7 @@ class Purchase(val items: String, val productManagement: ProductManagement) {
                     quantity
                 )
             ) { "[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요." }
-            cart[name] = cart.getOrDefault(name, quantity)
+            cart[name] = cart.getOrDefault(name, 0)+quantity
         }
     }
 
